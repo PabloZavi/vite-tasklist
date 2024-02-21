f446efa8bfb456eff5931f29b116eef007443938
PabloZ <pablozaviok@gmail.com>
2024-02-20 22:15:50 -0300

    Store methods created

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -17,6 +17,57 @@ const initStore = () => {
   console.log('Init Store');
 };
 
+const loadStore = () => {
+  throw new Error('Not implemented');
+};
+
+/**
+ *
+ * @param {String} description
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+};
+
+/**
+ *
+ * @param {String} todoId
+ */
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented');
+};
+
+/**
+ *
+ * @param {String} todoId
+ */
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented');
+};
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+};
+
+/**
+ *
+ * @param {String} newFilter
+ */
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+};
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+};
+
 export default {
   initStore,
+  loadStore,
+  addTodo,
+  toggleTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,
 };
